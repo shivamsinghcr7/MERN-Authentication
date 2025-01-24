7a607581d7a279f1cc62394bd6c635fa97da8680
Shivam Singh <shivam.iitk1@gmail.com>
2025-01-24 21:11:06 +0530

    apiSlice url update

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,13 +1,14 @@
 // thunk middleware is built-in
 // uses RTK query - library for interacting with backend API.
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RENDER_URL } from "../../url.js";
 
 // baseUrl is empty string because we've already set proxy as http://localhost:5000, otherwise we can set that here
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+//const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({ baseUrl: RENDER_URL });
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["User"],
   endpoints: (builder) => ({}),
 });
-
