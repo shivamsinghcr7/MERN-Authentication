20ac043c701295468f4792eef8928eab758d5740
Shivam Singh <shivam.iitk1@gmail.com>
2025-02-17 19:49:20 +0530

    Working on local server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
-
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
-import {Container } from "react-bootstrap";
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Container } from "react-bootstrap";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <Header />
-      <ToastContainer/>
+      <ToastContainer />
       <Container className="my-2">
         <Outlet />
       </Container>
